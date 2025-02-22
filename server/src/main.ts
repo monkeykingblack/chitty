c843685c9decd9305b87d2739241c079e24affa1
Trung Dang Quang <d.q.t.7775@gmail.com>
2025-02-22 14:15:32 +0700

    fix: wrong static folder

--- server/src/main.ts
+++ server/src/main.ts
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use('/api', routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/dist')));
+  app.use(express.static(path.join(__dirname, '../client')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
   });
 }
 
